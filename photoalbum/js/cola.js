8395f5c30a440575c75e78eab3b933bce27996ed
TaeCheon Na <ntc9058@gmail.com>
2021-05-02 15:29:07 +0900

    done

--- photoalbum/js/cola.js
+++ photoalbum/js/cola.js
@@ -1,6 +1,13 @@
-function colapic() {
+var cocolPics = document.querySelectorAll(".cocacolapic");
 
-    for(i=1; i<=23; i++) {
-     
-    }
+for (cocolPic of cocolPics) {
+    cocolPic.addEventListener("click", extensionPic, false);
+    cocolPic.addEventListener("mouseout", reductionPic, false);
 }
+function extensionPic() {   
+    this.style.width = "700px"; 
+}
+
+function reductionPic() {
+    this.style.width = "270px";
+}   
